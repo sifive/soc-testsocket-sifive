85755db6024d553c381fd2e6d1a878f4948cc754
Richard Xia <rxia@sifive.com>
2019-12-11 20:08:19 -0800

    Fix software boot method.
    This makes a change to the SkeletonDUT to add the Object Model instance for the
    memory attached to the mbus.
    
    This also explicitly assigns a name/description to that Object Model instance
    and updates the Wake function describing the boot method to target that memory
    name.

--- src/skeleton/SkeletonDUT.scala
+++ src/skeleton/SkeletonDUT.scala
@@ -32,8 +32,13 @@ class SkeletonDUT(harness: LazyScope)(implicit p: Parameters) extends RocketSubs
   def resetVector: BigInt = 0x80000000L
 
   // set eccBytes equal to beatBytes so we only generate a single memory
-  val main_mem_sram = LazyModule(new TLRAM(AddressSet(resetVector, 0x10000000-1), beatBytes = mbus.beatBytes,
-    ecc = ECCParams(bytes = mbus.beatBytes)))
+  val main_mem_sram = LazyModule(new TLRAM(
+    address = AddressSet(resetVector, 0x10000000-1),
+    beatBytes = mbus.beatBytes,
+    ecc = ECCParams(bytes = mbus.beatBytes),
+    parentLogicalTreeNode = Some(logicalTreeNode),
+    devName = Some("testsocket-main-memory")
+  ))
 
   main_mem_sram.node := TLFragmenter(mbus) := mbus.toDRAMController(Some("main_mem_sram"))()
 
