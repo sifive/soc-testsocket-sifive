f22fbb67d7dd49411c6654608bff168842898917
Henry Cook <henry@sifive.com>
2020-05-05 15:16:31 -0700

    skeleton: RCDefaultConfig supplies default bus topology

--- src/skeleton/SkeletonDUT.scala
+++ src/skeleton/SkeletonDUT.scala
@@ -24,19 +24,6 @@ trait HasAttachedBlocks { this: LazyModule =>
 
 class SkeletonDUT(harness: LazyScope)(implicit p: Parameters) extends RocketSubsystem with HasAttachedBlocks
 {
-  sbus.clockGroupNode := asyncClockGroupsNode
-  sbus.crossToBus(cbus, NoCrossing)
-  cbus.crossToBus(pbus, SynchronousCrossing())
-  FlipRendering { implicit p => sbus.crossFromBus(fbus, SynchronousCrossing()) }
-
-  private val BankedL2Params(nBanks, coherenceManager) = p(BankedL2Key)
-  private val (in, out, halt) = coherenceManager(this)
-  if (nBanks != 0) {
-    sbus.coupleTo("coherence_manager") { in :*= _ }
-    mbus.coupleFrom("coherence_manager") { _ :=* BankBinder(mbus.blockBytes * (nBanks-1)) :*= out }
-  }
-  mbus.clockGroupNode := sbus.clockGroupNode
-
   def resetVector: BigInt = 0x80000000L
 
   // set eccBytes equal to beatBytes so we only generate a single memory
