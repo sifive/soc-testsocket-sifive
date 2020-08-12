3fc167b3f0134e4d8897d584cfb9b05876d431dc
Wesley W. Terpstra <wesley@sifive.com>
2020-08-11 18:52:06 -0700

    Config: remove the MaskROM to boot directly from main memory

--- design/craft/fpga-test-socket/src/Configs.scala
+++ design/craft/fpga-test-socket/src/Configs.scala
@@ -5,7 +5,6 @@ import freechips.rocketchip.config.{Parameters, Config}
 import sifive.blocks.devices.gpio.{PeripheryGPIOKey, GPIOParams}
 import sifive.blocks.devices.spi.{PeripherySPIKey, SPIParams}
 import sifive.blocks.devices.uart.{PeripheryUARTKey, UARTParams}
-import freechips.rocketchip.devices.tilelink.MaskROMParams
 import freechips.rocketchip.subsystem._
 import sifive.skeleton.DefaultConfig
 
@@ -18,8 +17,6 @@ class FPGATestSocketPeripherals extends Config((site, here, up) => {
     SPIParams(rAddress = BigInt(0x64001000L)))
   case PeripheryGPIOKey => List(
     GPIOParams(address = BigInt(0x64002000L), width = 4))
-  case PeripheryMaskROMKey => List(
-    MaskROMParams(address = 0x10000, name = "BootROM"))
 })
 
 class FPGATestSocketConfig extends Config(
